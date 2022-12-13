7b02a33b7a7a424ed278825b51a66ef4931f1f4d
danielacmg <dcmg.ucv@gmail.com>
2022-12-13 03:01:30 +0000

    Domain name generator n-try

--- src/app.js
+++ src/app.js
@@ -2,10 +2,25 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+  let domain = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          domain = pronoun[i] + adj[j] + noun[k] + extension[l] + "\n";
+          console.log(domain + "\n");
+          document.getElementById("domainName").innerHTML += "</br>" + domain;
+        }
+      }
+    }
+  }
 };
